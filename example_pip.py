3134c5f54084d9401d474849ad6f70738da6f77c
Pranay Manocha <pmanocha@wash.cs.princeton.edu>
2020-10-06 01:24:52 -0400

    remove pip_pasm and example_pip.py

--- example_pip.py
+++
@@ -1,8 +0,0 @@
-import pip_pasm
-
-loss_fn = pip_pasm.PASM()
-wav_in = pip_pasm.load_audio('sample_audio/ref.wav')
-wav_out = pip_pasm.load_audio('sample_audio/2.wav')
-
-dist = loss_fn.forward(wav_in,wav_out)
-print(dist)
